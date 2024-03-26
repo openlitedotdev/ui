a5e5f0e0337975943a0d890b545b7a1694c38991
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-03-25 21:44:18 -0500

    feat(playground): add new package `lucide-react`

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,15 +1,55 @@
-import { Button } from '@design-system/react'
+import { Alert, AlertDescription, AlertTitle, Button } from '@design-system/react'
+import { CircleAlert, MailCheck, TriangleAlert } from 'lucide-react'
 
 function App() {
   return (
     <>
-      <main className="container mx-auto bg-success-400 text-white h-screen flex justify-center items-center gap-6 mt-10 rounded">
+      <main className="container mx-auto bg-purple-400/80 text-white h-screen flex justify-center items-center gap-6 mt-10 rounded flex-col">
+        <Button variant="success" size="lg">
+          Examples UI
+        </Button>
         <Button variant="primary" size="lg">
           Examples UI
         </Button>
-        <Button variant="danger" size="lg">
+        <Button variant="secondary" size="lg">
           Examples UI
         </Button>
+        <Button variant="error" size="lg">
+          Examples UI
+        </Button>
+        <Button>
+          Examples UI
+        </Button>
+        <Button variant="warn">
+          Examples UI
+        </Button>
+        <Alert variant="error">
+          <TriangleAlert className="size-6" />
+          <AlertTitle>
+            Hi Teams Open Land
+          </AlertTitle>
+          <AlertDescription>
+            This example component alert by Open Land
+          </AlertDescription>
+        </Alert>
+        <Alert variant="success">
+          <MailCheck className="size-6" />
+          <AlertTitle>
+            Hi Teams Open Land
+          </AlertTitle>
+          <AlertDescription>
+            This example component alert by Open Land
+          </AlertDescription>
+        </Alert>
+        <Alert variant="warn">
+          <CircleAlert className="size-6" />
+          <AlertTitle>
+            Hi Teams Open Land
+          </AlertTitle>
+          <AlertDescription>
+            This example component alert by Open Land
+          </AlertDescription>
+        </Alert>
       </main>
     </>
   )
