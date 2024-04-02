d6bfa831c9867ecec7130e64d0d22d8ce7bb4144
Gerzon05 <gerzonarletrangel@gmail.com>
2024-04-01 19:54:00 -0500

    fix(nextjs): changed the text color of the select component

--- packages/react/src/components/select.tsx
+++ packages/react/src/components/select.tsx
@@ -118,7 +118,7 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex w-full cursor-default select-none items-center rounded-md py-1.5 pl-2 pr-8 text-sm hover:bg-dark-700/50',
+      'relativ text-white eflex w-full cursor-default select-none items-center rounded-md py-1.5 pl-2 pr-8 text-sm hover:bg-dark-700/50',
       className,
     )}
     {...props}
