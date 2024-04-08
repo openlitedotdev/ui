dfaeee1170c82a4efb5871f4135edeb6ebfb99bc
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-08 13:21:43 -0500

    fix(with-nextjs): replace import `@design-system/react` to `@openui-org/react`

--- examples/with-nextjs/components/sheet-example.tsx
+++ examples/with-nextjs/components/sheet-example.tsx
@@ -22,7 +22,7 @@ export function SheetDemo() {
         <SheetHeader>
           <SheetTitle>Edit profile</SheetTitle>
           <SheetDescription>
-            Make changes to your profile here. Click save when you're done.
+            Make changes to your profile here. Click save when you&apos;re done.
           </SheetDescription>
         </SheetHeader>
         <div className="grid gap-4 py-4">
