6e390920db3d32463b77f6889f21e5fc7807238b
Sebastián García <johancs.mm@gmail.com>
2024-04-10 14:18:21 -0500

    feat: configure storybook enviroment (#119)

---
+++ apps/docs/.storybook/main.ts
@@ -0,0 +1,27 @@
+import { dirname, join } from 'node:path'
+
+function getAbsolutePath(value: string) {
+  return dirname(require.resolve(join(value, 'package.json')))
+}
+
+const config = {
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+  ],
+  framework: {
+    name: getAbsolutePath('@storybook/react-vite'),
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+}
+export default config
---
+++ apps/docs/tailwind.config.js
@@ -0,0 +1,10 @@
+import { react } from '@openui-org/react'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./stories/**/*.{html,js,jsx,tsx}', './node_modules/@openui-org/react/dist/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [react],
+}
