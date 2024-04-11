d9f617170d6b13eda30610abd68a575a7e77f233
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-10 21:04:16 -0500

    fix(web): remove attribute target in button

--- packages/react/src/plugin.ts
+++ packages/react/src/plugin.ts
@@ -1,4 +1,4 @@
-import plugin from 'tailwindcss/plugin.js'
+import plugin from 'tailwindcss/plugin'
 import { base, config, dark, ligth } from '@openui-org/theme'
 
 export const react = plugin(({ addBase }) => {
