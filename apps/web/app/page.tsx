8f3ca3d776295cb315bdeb0c89575173163d96d5
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-11 21:36:36 -0500

    fix(web): remove component `Demo.tsx`

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
   return (
     <>
       <Home />
-      <Demo />
       <Info />
     </>
   )
--- apps/web/components/Demo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function Demo() {
-  return (
-    <figure className="flex justify-center items-center sm:h-[36rem] mb-10">
-      <Image src="/demo-dashboard.png" alt="Demo dashboard with Open UI." className="rounded-md w-full object-cover" width="500" height="500" />
-    </figure>
-  )
-}
