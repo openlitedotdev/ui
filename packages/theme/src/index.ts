e4bc464ea714b5232c757ed07f40d842c15a05f3
Sebastián García <johancs.mm@gmail.com>
2024-04-11 20:15:53 -0500

    refactor(theme): add tsup.config.ts (#132)
    * refactor(theme): add `tsup.config.ts`

--- packages/theme/src/index.ts
+++ packages/theme/src/index.ts
@@ -1,2 +1,2 @@
-export * from './shadcn-ui'
+export * from './ui'
 export * from './tailwindcss'
---
+++ packages/theme/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig(options => ({
+  entryPoints: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  sourcemap: true,
+  ...options,
+}))
