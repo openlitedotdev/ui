6b97709558c5047bf368a38687ceab423a8695ce
castrogarciajs <johancs.mm@gmail.com>
2024-04-19 22:08:36 -0500

    feat(docs): add translate to presentation

--- docs/components/home.jsx
+++ docs/components/home.jsx
@@ -1,33 +1,27 @@
 import { Button } from "@openui-org/react/components";
 import Link from "next/link";
+import useLocalesMap from './use-locales'
+import {titleMap, descriptionMap, buttonPresentationMap} from '../i18n/translate'
 
 export default function Home() {
+  const title =  useLocalesMap(titleMap)
+  const description = useLocalesMap(descriptionMap)
+  const buttonPresentation = useLocalesMap(buttonPresentationMap)
+
   return (
     <section className="py-20 sm:py-24 relative">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl gap-16 sm:gap-y-24 flex flex-col">
+      <div className="mx-auto px-4 sm:px-6 max-w-7xl gap-16 sm:gap-y-24 flex flex-col">
         <div className="text-center relative z-[1]">
-          <h1 className="text-4xl sm:text-6xl font-bold tracking-tight text-balance">
-            <b className="text-sky"> Open UI </b>
-            . A modern design system
-            built with
-            <b className="text-sky"> Shadcn UI</b>
-            {' '}
-            and
-            <b className="text-sky"> Tailwind CSS </b>
+          <h1 className="text-6xl font-bold tracking-tight text-balance">
+           {title}
           </h1>
-          <p className="mt-6 text-base tracking-tight text-foreground text-pretty">
-            Open UI is an open source design system with a completely
-            cutting-edge system.
-            {' '}
-            <br />
-            {' '}
-            Fully customizable and modifiable you
-            will make UI development much faster with fully stylized components.
+          <p className="mt-6 opacity-90 text-sm tracking-tight text-pretty">
+           {description}
           </p>
           <div className="mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center">
             <Button asChild>
               <Link href="/docs/introduction">
-                Getting Started
+                {buttonPresentation}
               </Link>
             </Button>
           </div>
--- docs/i18n/translate.js
+++ docs/i18n/translate.js
@@ -6,8 +6,20 @@ export const languageMap = {
 
 /** @type {Readonly<Record<Locale, string>>} */
 export const titleMap = {
-  'en-US': 'Open UI . A modern design system built with Shadcn UI and Tailwind CSS',
+  'en-US': 'Open UI. A modern design system built with Shadcn UI and Tailwind CSS',
   'es-ES': 'Open UI. Un sistema de diseño moderno construido con Shadcn UI y Tailwind CSS',
+}
+
+/** @type {Readonly<Record<Locale, string>>} */
+export const descriptionMap = {
+  'en-US': 'Open UI is an open source design system with a completely cutting-edge system.  Fully customizable and modifiable you will make UI development much faster with fully stylized components.',
+
+  'es-ES': 'Open UI es un sistema de diseño de código abierto con un diseño completamente vanguardista. Totalmente personalizable y modificable hará que el desarrollo de UI sea mucho más rápido con componentes totalmente estilizados.',
+}
+
+export const buttonPresentationMap = {
+  'en-US': 'Getting Started',
+  'es-ES': 'Empezar',
 }
 
 /** @type {Readonly<Record<Locale, { utmSource: string; text: string; suffix?: string | undefined }>>} */
