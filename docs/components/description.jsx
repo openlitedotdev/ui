58e5d29cb617c15a662e7568e32c480be20b1871
gerzon05 <gerzonarletrangel@gmail.com>
2024-04-20 23:11:05 -0500

    docs: translate landing page presentation

--- docs/components/description.jsx
+++ docs/components/description.jsx
@@ -1,22 +1,30 @@
 import { Button } from '@openui-org/react/components'
+import {
+  titleMapPresentation,
+  buttonPresentationMap,
+  descriptionMapPresentation,
+} from 'i18n/translate'
+import useLocalesMap from './use-locales'
 import Link from 'next/link'
 
 export default function Description() {
+  const titlePresentation = useLocalesMap(titleMapPresentation)
+  const descriptionPresentation = useLocalesMap(descriptionMapPresentation)
+  const buttonPresentation = useLocalesMap(buttonPresentationMap)
   return (
     <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-zinc-800">
       <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
         <div className="space-y-2">
           <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-            Get Started with Open UI Design System
+            {titlePresentation}
           </h2>
           <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-            Explore our comprehensive documentation and start building your next
-            project with our design system.
+            {descriptionPresentation}
           </p>
         </div>
         <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
           <Button asChild>
-            <Link href="/docs/introduction">Explore Documentation</Link>
+            <Link href="/docs/introduction">{buttonPresentation}</Link>
           </Button>
         </div>
       </div>
