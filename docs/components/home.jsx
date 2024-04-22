5cccdb595e2de5c7667e6bb5112bddd2a0bdb27a
Sebastián García <johancs.mm@gmail.com>
2024-04-21 21:37:25 -0500

    perf: update home.jsx

--- docs/components/home.jsx
+++ docs/components/home.jsx
@@ -13,7 +13,7 @@ export default function Home() {
   const buttonPresentation = useLocalesMap(buttonPresentationMap)
 
   return (
-    <section className="w-full pt-12 md:pt-24 lg:pt-32 xl:pt-40">
+    <section className="w-full pt-12 md:pt-24 lg:pt-32">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:gap-16">
           <div className="space-y-4 md:space-y-6 lg:space-y-8">
@@ -27,9 +27,9 @@ export default function Home() {
           </div>
           <img
             alt="Hero"
-            className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full lg:aspect-square"
+            className="mx-auto aspect-video overflow-hidden rounded-xl object-cover sm:w-full"
             height="550"
-            src="/demo-commerce.png"
+            src="https://i.postimg.cc/X7yfn8hW/logo.png"
             width="550"
           />
         </div>
