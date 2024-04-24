0ec6e67ff20b6dff8b8d159aa0957fd89c1f16ed
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-04-24 15:51:10 -0500

    docs: add new component Tabs (#186)

--- docs/components/logo.jsx
+++ docs/components/logo.jsx
@@ -1,7 +1,9 @@
 export default function logo() {
   return (
     <h2 className="text-3xl font-bold">
-      Open <b className="text-sky">UI</b>
+      Open
+      {' '}
+      <b className="text-sky">UI</b>
     </h2>
   )
 }
