4c02d94dd49199fea968d1b912909724b0c1b03d
Sebastián García <johancs.mm@gmail.com>
2024-04-23 20:07:46 -0500

    docs: add component Alert (#177)
    * docs: add component `Alert`
    
    * formatter

--- docs/components/preview.jsx
+++ docs/components/preview.jsx
@@ -1,9 +1,16 @@
-export default function Preview({ children }) {
+import { cn } from 'lib/cn'
+
+export default function Preview({ children, className }) {
   return (
     <div className="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border">
       <div className="theme-zinc w-full">
         <div className="preview flex min-h-[350px] w-full justify-center p-20 items-center">
-          <div className="w-full flex items-center justify-center gap-4">
+          <div
+            className={cn(
+              'w-full flex items-center justify-center gap-4',
+              className,
+            )}
+          >
             {children}
           </div>
         </div>
