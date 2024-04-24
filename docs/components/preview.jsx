36b35204bf3808d542799bcfa5054117f15b73e0
Sebastián García <johancs.mm@gmail.com>
2024-04-24 13:50:47 -0500

    docs: add new component `Aspect Ratio` (#181)

--- docs/components/preview.jsx
+++ docs/components/preview.jsx
@@ -1,10 +1,15 @@
 import { cn } from 'lib/cn'
 
-export default function Preview({ children, className }) {
+export default function Preview({ children, className, classContainer }) {
   return (
     <div className="mt-2 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 relative rounded-md border">
-      <div className="theme-zinc w-full">
-        <div className="preview flex min-h-[350px] w-full justify-center p-20 items-center">
+      <div className="theme-zinc w-full h-full">
+        <div
+          className={cn(
+            'flex min-h-[350px] w-full justify-center p-20 items-center',
+            classContainer,
+          )}
+        >
           <div
             className={cn(
               'w-full flex items-center justify-center gap-4',
