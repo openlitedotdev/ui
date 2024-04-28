5bbc3d47fbdb15a4d49c4f6e00f78bc20bd3784c
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-27 19:14:59 -0500

    ci: ignore packages of examples

--- packages/components/react/src/components/alert/alert.tsx
+++ packages/components/react/src/components/alert/alert.tsx
@@ -1,8 +1,8 @@
 import { type VariantProps, alert, cn } from '@openui-org/theme'
 import * as React from 'react'
 
-interface Comp extends HTMLDivElement {}
-interface Props extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof alert> {}
+export interface Comp extends HTMLDivElement {}
+export interface Props extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof alert> {}
 
 const Alert = React.forwardRef<Comp, Props>(({ className, variant, ...props }, ref) => (
   <div
