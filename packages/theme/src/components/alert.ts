5bb68dd3c42395d92c9b4655e817d347c7feae94
Sebastián García <johancs.mm@gmail.com>
2024-04-27 22:08:11 -0500

    refactor: folder structure is changed (#200)

--- packages/theme/src/components/alert.ts
+++ packages/theme/src/components/alert.ts
@@ -1,19 +1,16 @@
 import { cva } from 'class-variance-authority'
 
-export const alert = cva(
-  'relative w-full rounded-large border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground',
-  {
-    variants: {
-      variant: {
-        default: 'bg-background',
-        error: 'border-error/60 dark:border-error [&>svg]:text-error',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
+export const alert = cva('relative w-full rounded-large border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground', {
+  variants: {
+    variant: {
+      default: 'bg-background',
+      error: 'border-error/60 dark:border-error [&>svg]:text-error',
     },
   },
-)
+  defaultVariants: {
+    variant: 'default',
+  },
+})
 
 export const alertDescription = cva('text-small [&_p]:leading-relaxed')
 
