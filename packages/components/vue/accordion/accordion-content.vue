4e36a8796960ac7833bcc39248f808ea221e1fbb
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-29 12:44:54 -0500

    feat: add starter vuejs library

---
+++ packages/components/vue/accordion/accordion-content.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from 'vue'
+import { AccordionContent, type AccordionContentProps } from 'radix-vue'
+import { accordionContent, cn } from '@openui-org/theme'
+
+const props = defineProps<AccordionContentProps & { class?: HTMLAttributes['class'] }>()
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props
+
+  return delegated
+})
+</script>
+
+<template>
+  <AccordionContent
+    v-bind="delegatedProps"
+    class="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+  >
+    <div :class="cn(accordionContent(), props.class)">
+      <slot />
+    </div>
+  </AccordionContent>
+</template>
---
+++ packages/components/vue/accordion/index.ts
@@ -0,0 +1,4 @@
+export { default as Accordion } from './accordion.vue'
+export { default as AccordionContent } from './accordion-content.vue'
+export { default as AccordionItem } from './accordion-item.vue'
+export { default as AccordionTrigger } from './accordion-trigger.vue'
