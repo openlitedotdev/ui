34c2ae62f1c6d233bc944bc89756588cf6a63057
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-05-03 17:35:03 -0500

    feat(vue): add stater with vuejs

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,5 @@ export default eslint({
   formatters: true,
   markdown: true,
   typescript: true,
+  vue: true,
 })
---
+++ packages/components/vue/button/button.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { button, cn } from '@openui-org/theme'
+
+interface Props {
+  variant?: NonNullable<Parameters<typeof button>[0]>['variant']
+  size?: NonNullable<Parameters<typeof button>[0]>['size']
+  as?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  as: 'button',
+})
+</script>
+
+<template>
+  <component
+    :is="as"
+    :class="cn(button({ variant, size }), $attrs.class ?? '')"
+  >
+    <slot />
+  </component>
+</template>
