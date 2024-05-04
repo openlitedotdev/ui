ee4a9e696e5fc0afaebf749534a43121a1960d9e
Sebastián García <johancs.mm@gmail.com>
2024-05-04 11:20:44 -0500

    feat: add link documentation

--- apps/web/components/explore-docs.tsx
+++ apps/web/components/explore-docs.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function ExploreDocs() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100">
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 flex flex-col items-center justify-center">
       <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
         <div className="space-y-2">
           <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
@@ -15,7 +15,7 @@ export default function ExploreDocs() {
         </div>
         <div className="flex flex-col gap-2 min-[400px]:flex-row lg:justify-end">
           <Button asChild>
-            <Link href="/docs/introduction">Explore Documentation</Link>
+            <Link href="https://openui-org.gitbook.io/openui/">Explore Documentation</Link>
           </Button>
         </div>
       </div>
