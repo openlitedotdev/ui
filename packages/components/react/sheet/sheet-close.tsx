357c954b7f1341cca393315a096992913863aae2
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-05-04 11:13:19 -0500

    feat(theme): added new component `Sheet` (#225)

---
+++ packages/components/react/sheet/sheet-close.tsx
@@ -0,0 +1,5 @@
+import * as SheetPrimitive from '@radix-ui/react-dialog'
+
+const SheetClose = SheetPrimitive.Close
+
+export default SheetClose
---
+++ packages/components/react/sheet/sheet-portal.tsx
@@ -0,0 +1,5 @@
+import * as SheetPrimitive from '@radix-ui/react-dialog'
+
+const SheetPortal = SheetPrimitive.Portal
+
+export default SheetPortal
