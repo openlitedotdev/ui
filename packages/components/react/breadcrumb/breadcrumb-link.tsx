7247eb20ddd239257ebb8a36ab0b570313ad0366
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-05-06 15:00:22 -0500

    fix(react): update prop asChild to breadcrumb-link

--- packages/components/react/breadcrumb/breadcrumb-link.tsx
+++ packages/components/react/breadcrumb/breadcrumb-link.tsx
@@ -3,21 +3,23 @@ import { Slot } from '@radix-ui/react-slot'
 import { breadcrumbLink, cn } from '@openui-org/theme'
 
 export interface Comp extends HTMLAnchorElement {}
-export interface Props extends React.ComponentPropsWithoutRef<'a'> {
+export interface Props extends React.ButtonHTMLAttributes<HTMLAnchorElement> {
   asChild?: boolean
 }
 
-const BreadcrumbLink = React.forwardRef<Comp, Props>(({ asChild = false, className, ...props }, ref) => {
-  const Comp = asChild ? Slot : 'a'
+const BreadcrumbLink = React.forwardRef<Comp, Props>(
+  ({ className, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'a'
+    return (
+      <Comp
+        className={cn(breadcrumbLink({ className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 
-  return (
-    <Comp
-      ref={ref}
-      className={cn(breadcrumbLink(), className)}
-      {...props}
-    />
-  )
-})
 BreadcrumbLink.displayName = 'BreadcrumbLink'
 
 export default BreadcrumbLink
