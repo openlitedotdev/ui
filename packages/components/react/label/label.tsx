16837d72e306cbdea4114826a29e874f1bbd6a28
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-05-06 15:13:18 -0500

    fix(react): update prop asChild to label

--- packages/components/react/label/label.tsx
+++ packages/components/react/label/label.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import type * as LabelPrimitive from '@radix-ui/react-label'
-import { cn, label } from '@openui-org/theme'
-import type { VariantProps } from '@openui-org/theme'
 import { Slot } from '@radix-ui/react-slot'
+import type { VariantProps } from '@openui-org/theme'
+import { cn, label } from '@openui-org/theme'
 
-interface Comp extends React.ElementRef<typeof LabelPrimitive.Root> {}
-export interface Props extends React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>,
-  VariantProps<typeof label> {
+export interface Comp extends HTMLLabelElement {}
+export interface Props extends React.LabelHTMLAttributes<HTMLLabelElement>, VariantProps<typeof label> {
   asChild?: boolean
 }
 
-const Label = React.forwardRef<Comp, Props>(({ className, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : 'label'
-  return (
-    <Comp
-      className={cn(label({ size, className }))}
-      ref={ref}
-      {...props}
-    />
-  )
-})
+const Label = React.forwardRef<Comp, Props>(
+  ({ className, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'label'
+    return (
+      <Comp
+        className={cn(label({ size, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  },
+)
 
 Label.displayName = 'Label'
 
