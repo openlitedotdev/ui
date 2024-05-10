3b6a17bd5ae873eb95beffe75da5e3e5569008c1
Sebastián García <johancs.mm@gmail.com>
2024-05-10 13:21:09 -0500

    feat: add new styles to the accordion (#268)

--- packages/theme/src/components/accordion.ts
+++ packages/theme/src/components/accordion.ts
@@ -10,7 +10,33 @@ import { cva } from 'class-variance-authority'
  *   // accordion elements
  * </Accordion>
  */
-export const accordion = cva('')
+export const accordion = cva('mt-2 p-3', {
+  variants: {
+    shadow: {
+      sm: 'shadow-small',
+      md: 'shadow-medium',
+      lg: 'shadow-large',
+      none: 'shadow-none',
+    },
+    radius: {
+      sm: 'rounded-small',
+      md: 'rounded-medium',
+      lg: 'rounded-large',
+      none: 'rounded-none',
+    },
+    bordered: {
+      sm: 'border-small',
+      md: 'border-medium',
+      lg: 'border-large',
+      none: 'border-none',
+    },
+  },
+  defaultVariants: {
+    shadow: 'none',
+    radius: 'sm',
+    bordered: 'none',
+  },
+})
 
 /**
  * Accordion wrapper **Class Variants** component
@@ -34,7 +60,41 @@ export const accordionContent = cva('overflow-hidden text-small transition-all d
  *   // accordion elements
  * </AccordionItem>
  */
-export const accordionItem = cva('border-b px-4')
+export const accordionItem = cva('px-4 py-2 my-3', {
+  variants: {
+    color: {
+      primary: 'bg-primary text-primary-foreground hover:bg-primary/90',
+      warn: 'bg-warn text-warn-foreground hover:bg-warn/90',
+      success: 'bg-success text-success-foreground hover:bg-success/90',
+      error: 'bg-error text-error-foreground hover:bg-error/90',
+      none: 'bg-transparent',
+    },
+    shadow: {
+      sm: 'shadow-small',
+      md: 'shadow-medium',
+      lg: 'shadow-large',
+      none: 'shadow-none',
+    },
+    radius: {
+      sm: 'rounded-small',
+      md: 'rounded-medium',
+      lg: 'rounded-large',
+      none: 'rounded-none',
+    },
+    separator: {
+      sm: 'border-b-small',
+      md: 'border-b-medium',
+      lg: 'border-b-large',
+      none: 'border-b-none',
+    },
+  },
+  defaultVariants: {
+    shadow: 'none',
+    radius: 'none',
+    color: 'none',
+    separator: 'none',
+  },
+})
 
 /**
  * Accordion wrapper **Class Variants** component
@@ -46,4 +106,15 @@ export const accordionItem = cva('border-b px-4')
  *   // accordion elements
  * </AccordionTrigger>
  */
-export const accordionTrigger = cva('flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180')
+export const accordionTrigger = cva('flex flex-1 items-center justify-between py-2 font-medium transition-all hover:opacity-80 [&[data-state=open]>svg]:rotate-180', {
+  variants: {
+    text: {
+      sm: 'text-small',
+      md: 'text-medium',
+      lg: 'text-large',
+    },
+  },
+  defaultVariants: {
+    text: 'md',
+  },
+})
