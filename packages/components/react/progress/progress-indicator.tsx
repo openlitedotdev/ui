bbe51c6f171df09d59a3ea3f8407d857177c9d35
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-05-11 17:53:40 -0500

    feat(theme): added new styles for component `Progress` (#273)

---
+++ packages/components/react/progress/progress-indicator.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react'
+import * as ProgressPrimitive from '@radix-ui/react-progress'
+import { cn, progressIndicator } from '@openui-org/theme'
+import type { VariantProps } from '@openui-org/theme'
+
+export interface Comp extends React.ElementRef<typeof ProgressPrimitive.Indicator> {}
+export interface Props extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Indicator>,
+  VariantProps<typeof progressIndicator> {
+  value: number
+}
+
+const ProgressIndicator = React.forwardRef<Comp, Props>(({ className, value, background }) => (
+  <ProgressPrimitive.Indicator
+    className={cn(progressIndicator({ background }), className)}
+    style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+  />
+))
+
+ProgressIndicator.displayName = ProgressPrimitive.Root.displayName
+
+export default ProgressIndicator
