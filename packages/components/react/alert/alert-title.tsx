3f57bc90e13c6281516bce0d1692e61469297614
Sebastián García <johancs.mm@gmail.com>
2024-05-15 21:42:10 -0500

    feat: update alert components with variant props (#280)

--- packages/components/react/alert/alert-title.tsx
+++ packages/components/react/alert/alert-title.tsx
@@ -1,13 +1,14 @@
+import type { VariantProps } from '@openui-org/theme'
 import { alertTitle, cn } from '@openui-org/theme'
 import React from 'react'
 
 export interface Comp extends HTMLParagraphElement {}
-export interface Props extends React.HTMLAttributes<HTMLHeadingElement> {}
+export interface Props extends React.HTMLAttributes<HTMLHeadingElement>, VariantProps<typeof alert> {}
 
-const AlertTitle = React.forwardRef<Comp, Props>(({ className, ...props }, ref) => (
+const AlertTitle = React.forwardRef<Comp, Props>(({ className, size, ...props }, ref) => (
   <h5
     ref={ref}
-    className={cn(alertTitle(), className)}
+    className={cn(alertTitle({ size }), className)}
     {...props}
   />
 ))
--- packages/theme/src/components/alert.ts
+++ packages/theme/src/components/alert.ts
@@ -10,15 +10,66 @@ import { cva } from 'class-variance-authority'
  *   // alert elements
  * </Alert>
  */
-export const alert = cva('relative w-full rounded-large border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground', {
+export const alert = cva('relative w-full p-4 [&>svg~*]:pl-8 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground', {
   variants: {
-    variant: {
-      default: 'bg-background',
-      error: 'border-error/60 dark:border-error [&>svg]:text-error',
+    outline: {
+      primary: 'border border-primary/30 dark:border-primary [&>svg]:text-primary text-primary/80',
+      error: 'border border-error/30 dark:border-error [&>svg]:text-error text-error/80',
+      warn: 'border border-warn/30 dark:border-warn [&>svg]:text-warn text-warn/80',
+      success: 'border border-success/30 dark:border-success [&>svg]:text-success text-success/80',
+      none: 'border border-transparent',
+    },
+    background: {
+      none: 'bg-transparent dark:bg-transparent',
+      primary: 'bg-primary/20 dark:bg-primary [&>svg]:text-primary text-primary/80 dark:text-primary-foreground dark:[&>svg]:text-white',
+      error: 'bg-error/20 [&>svg]:text-error text-error/80 dark:text-error-foreground dark:bg-error',
+      warn: 'bg-warn/20 [&>svg]:text-warn text-warn/80 dark:text-warn-foreground dark:bg-warn',
+      success: 'bg-success/20 [&>svg]:text-success text-success/80 dark:text-success-foreground dark:bg-success',
+    },
+
+    shadow: {
+      none: 'shadow-none',
+      sm: 'shadow-small',
+      md: 'shadow-medium',
+      lg: 'shadow-large',
+    },
+
+    rounded: {
+      none: 'rounded-none',
+      sm: 'rounded-small',
+      md: 'rounded-medium',
+      lg: 'rounded-large',
+    },
+
+  },
+  defaultVariants: {
+    background: 'primary',
+    shadow: 'sm',
+    rounded: 'sm',
+    outline: 'none',
+  },
+})
+
+/**
+ * Alert wrapper **Class Variants** component
+ *
+ * const styles = alertTitle({})
+ *
+ * @example
+ * <AvatarTitle role="alert" className={styles())}>
+ *   // alert title elements
+ * </AvatarTitle>
+ */
+export const alertTitle = cva('mb-2 font-semibold leading-none tracking-tight', {
+  variants: {
+    size: {
+      sm: 'text-small',
+      md: 'text-medium',
+      lg: 'text-large',
     },
   },
   defaultVariants: {
-    variant: 'default',
+    size: 'md',
   },
 })
 
@@ -32,16 +83,15 @@ export const alert = cva('relative w-full rounded-large border p-4 [&>svg~*]:pl-
  *   // alert description elements
  * </AlertDescription>
  */
-export const alertDescription = cva('text-small [&_p]:leading-relaxed')
-
-/**
- * Alert wrapper **Class Variants** component
- *
- * const styles = alertTitle({})
- *
- * @example
- * <AvatarTitle role="alert" className={styles())}>
- *   // alert title elements
- * </AvatarTitle>
- */
-export const alertTitle = cva('mb-1 font-medium leading-none tracking-tight')
+export const alertDescription = cva('[&_p]:leading-relaxed', {
+  variants: {
+    size: {
+      sm: 'text-small',
+      md: 'text-medium',
+      lg: 'text-large',
+    },
+  },
+  defaultVariants: {
+    size: 'sm',
+  },
+})
