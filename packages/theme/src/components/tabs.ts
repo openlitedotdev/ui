ddd6c8c7b74a4927dc1c39df69b4013e5e3430d8
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-05-18 10:34:24 -0500

    feat(theme): added new styles for component `Tabs` (#286)

--- packages/theme/src/components/tabs.ts
+++ packages/theme/src/components/tabs.ts
@@ -22,7 +22,7 @@ export const tabs = cva('')
  *   // TabsList elements
  * </TabsList>
  */
-export const tabsList = cva('inline-flex w-full h-full items-center justify-center rounded-medium bg-muted p-1 text-muted-foreground')
+export const tabsList = cva('inline-flex w-full p-2 h-full items-center justify-center rounded-medium bg-foreground/10')
 
 /**
  * Tabs wrapper **Class Variants** component
@@ -34,7 +34,20 @@ export const tabsList = cva('inline-flex w-full h-full items-center justify-cent
  *   // TabsTrigger elements
  * </TabsTrigger>
  */
-export const tabsTrigger = cva('inline-flex w-1/2 items-center justify-center whitespace-nowrap rounded-small p-3 text-medium font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm')
+export const tabsTrigger = cva('inline-flex w-fill items-center justify-center whitespace-nowrap rounded-small p-3 text-medium font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-white data-[state=active]:shadow-sm', {
+  variants: {
+    background: {
+      none: 'data-[state=active]:bg-white data-[state=active]:dark:bg-black',
+      primary: 'data-[state=active]:bg-primary',
+      success: 'data-[state=active]:bg-success',
+      error: 'data-[state=active]:bg-error',
+      warn: 'data-[state=active]:bg-warn',
+    },
+  },
+  defaultVariants: {
+    background: 'none',
+  },
+})
 
 /**
  * Tabs wrapper **Class Variants** component
@@ -46,4 +59,4 @@ export const tabsTrigger = cva('inline-flex w-1/2 items-center justify-center wh
  *   // TabsContent elements
  * </TabsContent>
  */
-export const tabsContent = cva('mt-2 p-3 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-medium border border-muted')
+export const tabsContent = cva('mt-2 p-3 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 rounded-medium border border-foreground/10')
