bdf8c8c6464522a9b37294d44f260cc7f4989945
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-05-19 11:29:27 -0500

    feat(theme): fixed text color for component `Select` (#302)
    Resolves #296

--- packages/theme/src/components/select.ts
+++ packages/theme/src/components/select.ts
@@ -46,7 +46,7 @@ export const selectValue = cva('')
  *   // SelectTrigger elements
  * </SelectTrigger>
  */
-export const selectTrigger = cva('flex relative bg-background dark:bg-white/10 h-10 w-64 items-center justify-between rounded-medium px-3 py-2 text-small ring-offset-transparent focus:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1', {
+export const selectTrigger = cva('flex relative bg-default h-10 w-64 items-center justify-between rounded-medium px-3 py-2 text-small ring-offset-transparent focus:outline-none focus:ring-2 focus:ring-transparent focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1', {
   variants: {
     shadow: {
       sm: 'shadow-small',
@@ -93,7 +93,7 @@ export const selectScrollDownButton = cva('flex cursor-default items-center just
  *   // SelectContent elements
  * </SelectContent>
  */
-export const selectContent = cva('relative top-1 w-64 bg-background dark:bg-white/10 left-0 z-50 overflow-hidden rounded-medium shadow-medium data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2')
+export const selectContent = cva('relative top-1 w-64 bg-default left-0 z-50 overflow-hidden rounded-medium shadow-medium data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2')
 
 /**
  * Select wrapper **Class Variants** component
@@ -117,7 +117,7 @@ export const selectLabel = cva('py-1.5 px-8 text-small font-semibold')
  *   // SelectItem elements
  * </SelectItem>
  */
-export const selectItem = cva('relative flex w-full cursor-default select-none items-center rounded-small py-1.5 px-8 text-small outline-none hover:bg-neutral data-[disabled]:pointer-events-none data-[disabled]:opacity-50 focus-within:bg-neutral')
+export const selectItem = cva('relative flex w-full cursor-default select-none items-center rounded-small py-1.5 px-8 text-small outline-none hover:bg-white/30 data-[disabled]:pointer-events-none data-[disabled]:opacity-50 focus-within:bg-white/30')
 
 /**
  * Select wrapper **Class Variants** component
@@ -127,4 +127,4 @@ export const selectItem = cva('relative flex w-full cursor-default select-none i
  * @example
  * <SelectSeparator role="group" className={styles())} />
  */
-export const selectSeparator = cva('-mx-1 my-1 h-px bg-neutral')
+export const selectSeparator = cva('-mx-1 my-1 h-px bg-default')
