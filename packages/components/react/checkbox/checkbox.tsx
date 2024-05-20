4d2dbb017348638ef59b576a40dc3159eb6070f1
castrogarciajs <johancs.mm@gmail.com>
2024-05-19 20:39:28 -0500

    feat: optimize Checkbox component styles

--- packages/components/react/checkbox/checkbox.tsx
+++ packages/components/react/checkbox/checkbox.tsx
@@ -1,20 +1,21 @@
 import * as React from 'react'
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
+import type { VariantProps } from '@openui-org/theme'
 import { checkbox, cn } from '@openui-org/theme'
 
 export interface Comp extends React.ElementRef<typeof CheckboxPrimitive.Root> {}
-export interface Props extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {}
+export type Props = React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> & VariantProps<typeof checkbox>
 
-const Checkbox = React.forwardRef<Comp, Props>(({ className, ...props }, ref) => (
+const Checkbox = React.forwardRef<Comp, Props>(({ className, color, rounded, ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
-    className={cn(checkbox(), className)}
+    className={cn(checkbox({ color, rounded }), className)}
     {...props}
   >
-    <CheckboxPrimitive.Indicator
-      className={cn('flex items-center justify-center text-current')}
-    >
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-check size-4"><path d="M20 6 9 17l-5-5" /></svg>
+    <CheckboxPrimitive.Indicator>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+        <path d="M5 14.5C5 14.5 6.5 14.5 8.5 18C8.5 18 14.0588 8.83333 19 7" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="size-4" />
+      </svg>
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ))
--- packages/theme/src/components/checkbox.ts
+++ packages/theme/src/components/checkbox.ts
@@ -8,4 +8,24 @@ import { cva } from 'class-variance-authority'
  * @example
  * <Checkbox className={styles())} />
  */
-export const checkbox = cva('peer size-4 shrink-0 rounded-small border focus-visible:outline-none focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground')
+export const checkbox = cva('peer size-4 shrink-0 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', {
+  variants: {
+    color: {
+      default: 'data-[state=checked]:bg-default data-[state=checked]:text-default-foreground border border-default',
+      primary: 'data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground border border-primary',
+      success: 'data-[state=checked]:bg-success data-[state=checked]:text-success-foreground border border-success',
+      error: 'data-[state=checked]:bg-error data-[state=checked]:text-error-foreground border border-error',
+      warn: 'data-[state=checked]:bg-warn data-[state=checked]:text-warn-foreground border border-warn',
+    },
+    rounded: {
+      none: 'rounded-none',
+      sm: 'rounded-small',
+      md: 'rounded-medium',
+      lg: 'rounded-large',
+    },
+  },
+  defaultVariants: {
+    color: 'default',
+    rounded: 'sm',
+  },
+})
