a284d294317c2c64c61443acadcb1161a2c0648f
Gerzon05 <gerzonarletrangel@gmail.com>
2024-05-20 15:55:31 -0500

    fix(theme): changed color for circule

--- packages/components/react/slider/slider.tsx
+++ packages/components/react/slider/slider.tsx
@@ -24,7 +24,7 @@ const Slider = React.forwardRef<Comp, Props>(({ className, styles, background = 
     <SliderTrack>
       <SliderRange background={background} className={styles} />
     </SliderTrack>
-    <SliderPrimitive.Thumb className={`block h-5 w-5 rounded-full border-2 border-${background} bg-default ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-0`} />
+    <SliderPrimitive.Thumb className={`block h-5 w-5 rounded-full border-2 border-${background} bg-${background} shadow-${background} ring-offset-${background} transition-colors focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`} />
   </SliderPrimitive.Root>
 ))
 
