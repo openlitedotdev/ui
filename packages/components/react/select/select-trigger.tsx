9f565282abc7f2f0dd71ec7f2072beb263a12bd1
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-05-19 21:21:13 -0500

    feat(react): migrate icons for component `Select` (#304)

--- packages/components/react/select/select-trigger.tsx
+++ packages/components/react/select/select-trigger.tsx
@@ -16,7 +16,9 @@ const SelectTrigger = React.forwardRef<Comp, Props>(({ className, shadow, childr
     >
       {children}
       <SelectPrimitive.Icon asChild>
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chevron-down"><path d="m6 9 6 6 6-6" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="foreground" fill="none">
+          <path d="M18 9.00005C18 9.00005 13.5811 15 12 15C10.4188 15 6 9 6 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
       </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
   )
