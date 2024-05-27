f98c9ac1073a6e3ddd33cafc78a9e404c25c7e3d
Sebastián García <159331563+sebastian-mjs@users.noreply.github.com>
2024-05-27 11:01:16 -0500

    fix: update icon mobile for support dark mode

--- apps/web/components/movil-sidebar-nav.tsx
+++ apps/web/components/movil-sidebar-nav.tsx
@@ -10,7 +10,7 @@ export default function MovilSidebar() {
       <SheetTrigger asChild>
         <Button color="ghost" size="icon" className="relative bg-transparent border border-default md:hidden h-9 px-5">
           <span className=" absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="currentColor" fill="none">
               <path d="M4 5L20 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
               <path d="M4 12L20 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
               <path d="M4 19L20 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
