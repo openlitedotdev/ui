9e133da36a7272c3640c9bd0306a517eede3be04
Gerzon05 <gerzonarletrangel@gmail.com>
2024-05-27 10:32:51 -0500

    feat(docs): new styles for button and icons

--- apps/web/components/movil-sidebar-nav.tsx
+++ apps/web/components/movil-sidebar-nav.tsx
@@ -8,20 +8,12 @@ export default function MovilSidebar() {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
-        <Button color="ghost" size="icon" className="relative md:hidden ml-auto">
+        <Button color="ghost" size="icon" className="relative bg-transparent border border-default md:hidden h-9 px-5">
           <span className=" absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-6"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#000000" fill="none">
+              <path d="M4 5L20 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M4 12L20 12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+              <path d="M4 19L20 19" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
             </svg>
           </span>
         </Button>
