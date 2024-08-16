71541ee3890485e00e550a29ac2dae789a18d618
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-08-16 18:35:20 -0500

    feat: style for the Input component (#355)

--- src/components/forms/input/input.tsx
+++ src/components/forms/input/input.tsx
@@ -8,16 +8,16 @@ export interface Props extends React.InputHTMLAttributes<HTMLInputElement>,
   labelText?: string
 }
 
-const Input = React.forwardRef<Comp, Props>(({ className, border, background, labelText, type, ...props }, ref) => {
+const Input = React.forwardRef<Comp, Props>(({ className, rounded, icons, variant, labelText, type, ...props }, ref) => {
   return (
     <div className="relative flex flex-col-reverse">
       <input
         type={type}
-        className={cn(input({ border, background }), className)}
+        className={cn(input({ variant, icons, rounded }), className)}
         ref={ref}
         {...props}
       />
-      <label className="text-foreground/60 absolute top-1/2 -translate-y-1/2 left-4 px-1 bg-background pointer-events-none text-medium peer-focus-within:text-medium peer-focus-within:top-0 transition-all">{labelText}</label>
+      <label className="absolute top-0 -translate-y-1/2 left-4 pointer-events-none text-small backdrop-blur-sm px-1">{ labelText }</label>
     </div>
 
   )
