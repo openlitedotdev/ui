30e5cd233dade33f38d8a2baf7f6b72e5a5d9345
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-16 09:42:55 -0500

    feat: style for the Input component

--- src/plugins/components/input.ts
+++ src/plugins/components/input.ts
@@ -8,25 +8,40 @@ import { cva } from 'class-variance-authority'
  * @example
  * <Input className={styles())} />
  */
-export const input = cva('h-10 w-full peer transition-colors rounded-medium border-2 bg-transparent px-4 py-2 text-medium ring-offset-background file:border-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0', {
+export const input = cva('h-10 w-full transition-colors rounded-medium border-2 bg-transparent px-4 py-2 text-medium ring-offset-background file:border-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0', {
   variants: {
     border: {
-      none: 'border-foreground/30 focus-visible:border-foreground/60',
-      primary: 'border-primary/50 focus-visible:border-primary',
-      success: 'border-success/50 focus-visible:border-success',
-      warn: 'border-warn/50 focus-visible:border-warn',
-      error: 'border-error/50 focus-visible:border-error',
+      default: 'border-default/30 focus-visible:border-default transition-border durations-500',
+      primary: 'border-primary/30 focus-visible:border-primary transition-border durations-500',
+      success: 'border-success/30 focus-visible:border-success transition-border durations-500',
+      warn: 'border-warn/30 focus-visible:border-warn transition-border durations-500',
+      error: 'border-error/30 focus-visible:border-error transition-border durations-500',
     },
-    background: {
+    variant: {
       none: 'bg-transparent',
-      primary: 'bg-primary/30',
-      success: 'bg-success/30',
-      warn: 'bg-warn/30',
-      error: 'bg-error/30',
+      default: 'bg-default/10',
+      primary: 'bg-primary/10',
+      success: 'bg-success/10',
+      warn: 'bg-warn/10',
+      error: 'bg-error/10',
+    },
+    icons: {
+      none: '',
+      left: 'pl-10',
+      right: 'pr-10',
+      both: 'px-10',
+    },
+    rounded: {
+      none: 'rounded-none',
+      small: 'rounded-small',
+      medium: 'rounded-medium',
+      large: 'rounded-large',
     },
   },
   defaultVariants: {
-    border: 'none',
-    background: 'none',
+    border: 'default',
+    variant: 'none',
+    icons: 'none',
+    rounded: 'none',
   },
 })
