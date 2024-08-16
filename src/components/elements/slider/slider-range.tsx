491ec2e419c0773bc601abd64c89ed8d5f368a11
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-16 16:33:49 -0500

    feat: style for the slider component

--- src/components/elements/slider/slider-range.tsx
+++ src/components/elements/slider/slider-range.tsx
@@ -6,10 +6,10 @@ import type { VariantProps } from '@/tailwind'
 export interface Comp extends React.ElementRef<typeof SliderPrimitive.Range> {}
 export interface Props extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Range>, VariantProps<typeof sliderRange> {}
 
-const SliderRange = React.forwardRef<Comp, Props>(({ className, background, ...props }, ref) => (
+const SliderRange = React.forwardRef<Comp, Props>(({ className, variant, ...props }, ref) => (
   <SliderPrimitive.Range
     ref={ref}
-    className={cn(sliderRange({ background }), className)}
+    className={cn(sliderRange({ variant }), className)}
     {...props}
   />
 ))
--- src/components/elements/slider/slider.tsx
+++ src/components/elements/slider/slider.tsx
@@ -8,25 +8,54 @@ import { cn, slider } from '@/tailwind'
 export interface Comp extends React.ElementRef<typeof SliderPrimitive.Root> {}
 export interface Props extends React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>,
   VariantProps<typeof slider> {
-  background?: 'error' | 'primary' | 'success' | 'warn' | null | undefined
+  variant?: 'error' | 'primary' | 'success' | 'warn' | null | undefined
   styles?: string
+  values?: boolean
 }
 
-const Slider = React.forwardRef<Comp, Props>(({ className, styles, background = 'primary', ...props }, ref) => (
-  <SliderPrimitive.Root
-    ref={ref}
-    className={cn(
-      slider(),
-      className,
-    )}
-    {...props}
-  >
-    <SliderTrack>
-      <SliderRange background={background} className={styles} />
-    </SliderTrack>
-    <SliderPrimitive.Thumb className={`block h-5 w-5 rounded-full border-2 border-${background} bg-${background} shadow-${background} ring-offset-${background} transition-colors focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`} />
-  </SliderPrimitive.Root>
-))
+const Slider = React.forwardRef<Comp, Props>(({ className, styles, values = false, variant = 'primary', ...props }, ref) => {
+  const [value, setValue] = React.useState<number[]>(props.defaultValue || [0])
+
+  return (
+    <div className="w-full">
+      <SliderPrimitive.Root
+        ref={ref}
+        className={cn(
+          slider(),
+          className,
+        )}
+        {...props}
+        value={value}
+        onValueChange={newValue => setValue(newValue)}
+      >
+        <SliderTrack>
+          <SliderRange variant={variant} className={styles} />
+        </SliderTrack>
+        <SliderPrimitive.Thumb className="block size-6 flex justify-center rounded-full bg-white border shadow-medium transition-colors focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+          {
+            values
+              ? (
+                  <p className="-mt-5">
+                    {value[0]}
+                  </p>
+                )
+              : ''
+          }
+        </SliderPrimitive.Thumb>
+      </SliderPrimitive.Root>
+      {
+        values
+          ? (
+              <div className="flex w-full justify-between mt-2">
+                <p className="text-black">{props.min ? props.min : 0}</p>
+                <p className="text-black">{props.max ? props.max : 100}</p>
+              </div>
+            )
+          : ''
+      }
+    </div>
+  )
+})
 
 Slider.displayName = SliderPrimitive.Root.displayName
 
