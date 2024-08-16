4748888dbf75005441ee8f5c4282c423cbcebdc0
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-16 16:38:15 -0500

    fix: delete variant for background

--- src/components/forms/input/input.tsx
+++ src/components/forms/input/input.tsx
@@ -8,12 +8,12 @@ export interface Props extends React.InputHTMLAttributes<HTMLInputElement>,
   labelText?: string
 }
 
-const Input = React.forwardRef<Comp, Props>(({ className, border, rounded, icons, variant, labelText, type, ...props }, ref) => {
+const Input = React.forwardRef<Comp, Props>(({ className, rounded, icons, variant, labelText, type, ...props }, ref) => {
   return (
     <div className="relative flex flex-col-reverse">
       <input
         type={type}
-        className={cn(input({ border, variant, icons, rounded }), className)}
+        className={cn(input({ variant, icons, rounded }), className)}
         ref={ref}
         {...props}
       />
--- src/plugins/components/input.ts
+++ src/plugins/components/input.ts
@@ -10,20 +10,12 @@ import { cva } from 'class-variance-authority'
  */
 export const input = cva('h-10 w-full transition-colors rounded-medium border-2 bg-transparent px-4 py-2 text-medium ring-offset-background file:border-0 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0', {
   variants: {
-    border: {
+    variant: {
       default: 'border-default/30 focus-visible:border-default transition-border durations-500',
       primary: 'border-primary/30 focus-visible:border-primary transition-border durations-500',
       success: 'border-success/30 focus-visible:border-success transition-border durations-500',
       warn: 'border-warn/30 focus-visible:border-warn transition-border durations-500',
       error: 'border-error/30 focus-visible:border-error transition-border durations-500',
-    },
-    variant: {
-      none: 'bg-transparent',
-      default: 'bg-default/10',
-      primary: 'bg-primary/10',
-      success: 'bg-success/10',
-      warn: 'bg-warn/10',
-      error: 'bg-error/10',
     },
     icons: {
       none: '',
@@ -39,8 +31,7 @@ export const input = cva('h-10 w-full transition-colors rounded-medium border-2 
     },
   },
   defaultVariants: {
-    border: 'default',
-    variant: 'none',
+    variant: 'default',
     icons: 'none',
     rounded: 'none',
   },
