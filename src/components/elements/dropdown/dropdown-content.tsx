31def22cf9088c64a922aaba642d39c32ca63f22
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-08-17 13:57:37 -0500

    feat: styles for dropdown component (#365)

--- src/components/elements/dropdown/dropdown-content.tsx
+++ src/components/elements/dropdown/dropdown-content.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import * as DropdownPrimitive from '@radix-ui/react-dropdown-menu'
 import DropdownPortal from './dropdown-portal'
+import { ScrollArea } from '@/components/data/scroll-area'
 import { cn, dropdownContent } from '@/tailwind'
 
 export interface Comp extends React.ElementRef<typeof DropdownPrimitive.Content> {}
@@ -13,7 +14,11 @@ const DropdownMenuContent = React.forwardRef<Comp, Props>(({ className, sideOffs
       sideOffset={sideOffset}
       className={cn(dropdownContent(), className)}
       {...props}
-    />
+    >
+      <ScrollArea className="w-full max-h-72">
+        {props.children}
+      </ScrollArea>
+    </DropdownPrimitive.Content>
   </DropdownPortal>
 ))
 
