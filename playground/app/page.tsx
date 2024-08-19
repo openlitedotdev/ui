5194a3cd2209ed27b7591ccde0f0df88d8512fec
Sebastián García <johancs.mm@gmail.com>
2024-08-19 14:46:42 -0500

    feat: styles for Dialog component (#366)

--- playground/app/page.tsx
+++ playground/app/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from '@openlabs/ui'
-import { Plus } from 'lucide-react'
-
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center w-screen h-screen gap-y-4 p-24">
       {/** Add your example, before remove */}
-      <Button outline="default" rounded="full">Hola, Mundo</Button>
-      <Button size="icon" outline="primary" shadow="none" rounded="full">
-        <Plus />
-      </Button>
     </main>
   )
 }
