bf58f033ba60787a8984817c53af2d65e173c29a
Gerzon Rangel <128724547+gerzon05@users.noreply.github.com>
2024-08-20 17:54:09 -0500

    Update tooltip.ts

--- src/plugins/components/tooltip.ts
+++ src/plugins/components/tooltip.ts
@@ -45,4 +45,4 @@ export const tooltipProvider = cva('')
  *   // Tooltip elements
  * </TooltipContent>
  */
-export const tooltipContent = cva('z-40 w-fit max-w-72 px-2 py-1 !text-foreground bg-background border border-default rounded-medium text-small shadow-small animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2')
+export const tooltipContent = cva('z-50 w-fit max-w-72 px-2 py-1 !text-foreground bg-background border border-default rounded-medium text-small shadow-small animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2')
