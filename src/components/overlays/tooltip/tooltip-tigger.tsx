5c1bcc56a14216f93b1b54128202b71a1b298117
Gerzon05 <gerzonarletrangel@gmail.com>
2024-08-20 09:29:10 -0500

    fix: delete variant for tooltipTrigger

--- src/components/overlays/tooltip/tooltip-tigger.tsx
+++ src/components/overlays/tooltip/tooltip-tigger.tsx
@@ -6,11 +6,11 @@ import type { VariantProps } from '@/tailwind'
 interface Comp extends React.ElementRef<typeof TooltipPrimitive.Trigger> {}
 export interface Props extends React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Trigger>, VariantProps<typeof tooltipTrigger> {}
 
-const TooltipTrigger = React.forwardRef<Comp, Props>(({ className, variant, ...props }, ref) => {
+const TooltipTrigger = React.forwardRef<Comp, Props>(({ className, ...props }, ref) => {
   return (
     <TooltipPrimitive.Trigger
       ref={ref}
-      className={cn(tooltipTrigger({ variant }), className)}
+      className={cn(tooltipTrigger(), className)}
       {...props}
     />
   )
