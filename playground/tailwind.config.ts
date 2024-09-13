3e7d5675ebd5f9810b4e2243b8b21a8d08f2d1aa
Sebastián García <johancs.mm@gmail.com>
2024-09-12 21:40:39 -0500

    feat: improve style to component dropdown (#392)

--- playground/tailwind.config.ts
+++ playground/tailwind.config.ts
@@ -1,12 +1,12 @@
 import type { Config } from 'tailwindcss'
-import { openui } from '@openlabs/ui/tailwind'
+import { openui } from '@openlite/ui/tailwind'
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@openlabs/ui/dist/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@openlite/ui/dist/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
