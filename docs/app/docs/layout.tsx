5e9595b9678dcf418f83f59043711fb3b706e06d
castrogarciajs <johancs.mm@gmail.com>
2024-09-12 20:27:48 -0500

    docs: create favicon app router

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -3,5 +3,12 @@ import type { ReactNode } from 'react'
 import { docsOptions } from '../layout.config'
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <DocsLayout {...docsOptions}>{children}</DocsLayout>
+  return (
+    <>
+      <aside className="sticky py-2 bg-warn text-warn-foreground text-center">
+        <h4 className="font-bold">Documentation is currently in beta</h4>
+      </aside>
+      <DocsLayout {...docsOptions}>{children}</DocsLayout>
+    </>
+  )
 }
