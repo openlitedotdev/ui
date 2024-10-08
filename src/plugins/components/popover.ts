c567060b927b292948e54e4202917fff288d0f95
Gerzon05 <gerzonarletrangel@gmail.com>
2024-10-07 21:04:00 -0500

    docs: component popover

--- src/plugins/components/popover.ts
+++ src/plugins/components/popover.ts
@@ -34,7 +34,7 @@ export const popoverTrigger = cva('')
  *   // PopoverContent elements
  * </PopoverContent>
  */
-export const popoverContent = cva('z-50 w-72 rounded-medium bg-foreground dark:bg-background border border-foreground/10 p-4 text-foreground shadow-medium outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2', {
+export const popoverContent = cva('z-50 w-72 rounded-medium bg-background border border-foreground/10 p-4 shadow-medium outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2', {
   variants: {
     shadow: {
       sm: 'shadow-small',
